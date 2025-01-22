37085b6eda0aa522fbe6d1b2a8a457d1917277da
Bipin Oli <bipinoli90@gmail.com>
2025-01-22 18:16:41 +0100

    WIP - cycle that can't be pipelined

--- src/templates/cycle_with_tricky_offset/cycle_with_tricky_offset.hs
+++ src/templates/cycle_with_tricky_offset/cycle_with_tricky_offset.hs
@@ -19,7 +19,7 @@ type ValidOutput = Bool
 type MemoryOutput = (Empty, ValidOutput, MemoryData)
 
 defaultVal :: MemoryData
-defaultVal = (0, False, False, False, False)
+defaultVal = 0
 
 fifoFx :: MemoryState -> MemoryInput -> (MemoryState, MemoryOutput)
 fifoFx memoryState memoryInput = (nextMemoryState, output)
@@ -65,24 +65,39 @@ hotPotato n = let s = register 0 (mux (s .==. pure (n-1)) 0 (s + 1)) in s
 
 ---------------------------------------------------------------
 
--- NOTE: How to handle pacing & step by step layerwise evaluation together
--- Idea 1: Run evalution at higher frequency than pacing (similar idea to existing compiler)
--- Idea 2: Predict when the stream will be evaluatated and provide the dealayed pacing matching that -> whole thing can be run in higher frequency
-evaluator :: HiddenClockResetEnable dom => Signal dom (Int, Bool) -> Signal dom ((Int, Bool), (Int, Bool), (Int, Bool))
-evaluator input0 = bundle (output0, output1, output2)
-    where
-        output0 = (a, aktv_a)
-        output1 = (b, aktv_b)
-        output2 = (c, aktv_c)
-        (x, aktv_x) = input0
-        (a, aktv_a) = evaluateA enA x c
-        (b, aktv_b) = evaluateB enb a
-        (c, aktv_c) = evaluateC enC b
-        enA = aktv_x
-        enB = delay False enA
-        enC = delay False enB
-        ...
+-- -- NOTE: How to handle pacing & step by step layerwise evaluation together
+-- -- Idea 1: Run evalution at higher frequency than pacing (similar idea to existing compiler)
+-- -- Idea 2: Predict when the stream will be evaluatated and provide the dealayed pacing matching that -> whole thing can be run in higher frequency
+-- evaluator :: HiddenClockResetEnable dom => Signal dom (Int, Bool) -> Signal dom ((Int, Bool), (Int, Bool), (Int, Bool))
+-- evaluator input0 = bundle (output0, output1, output2)
+--     where
+--         output0 = (a, aktv_a)
+--         output1 = (b, aktv_b)
+--         output2 = (c, aktv_c)
+--         (x, aktv_x) = input0
+--         (a, aktv_a) = evaluateA enA (x, c)
+--         (b, aktv_b) = evaluateB enb a
+--         (c, aktv_c) = evaluateC enC b
+--         enA = aktv_x
+--         enB = delay False enA
+--         enC = delay False enB
 
 
+evaluateA :: HiddenClockResetEnable dom => Signal dom Bool -> Signal dom (Int, Int) -> Signal dom (Int, Bool)
+evaluateA en inpt = register (0, False) (mux en newResult oldResult)
+    where 
+        newResult = bundle (newVal, pure True)
+        oldResult = bundle (oldVal, pure False)
+        newVal = x + c
+        (x, c) = unbundle inpt 
+        (oldVal, _) = unbundle $ evaluateA en inpt
+
+-- evaluateB :: HiddenClockResetEnable dom => Signal dom Bool -> Signal dom Int -> Signal dom Int
+-- evaluateB en inpt = register (0, False) (mux en newResult oldResult)
+--     where
+--         newResult = bundle (newVal, True)
+--         oldResult = bundle (oldVal, False)
+--         (oldVal, _) = evaluateB en inpt
+--         (newVal, _) = bundle (inpt + 1, pure True)
 
 
