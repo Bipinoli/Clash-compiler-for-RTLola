a65539f83955d400e9248df96cff7d8d172f5b83
Bipin Oli <bipinoli90@gmail.com>
2025-01-23 12:39:08 +0100

    WIP - prototype ready - need to test now

--- src/templates/cycle_with_tricky_offset/cycle_with_tricky_offset.hs
+++ src/templates/cycle_with_tricky_offset/cycle_with_tricky_offset.hs
@@ -2,56 +2,24 @@ module CycleWithTrickyOffset where
 
 import Clash.Prelude
 
+
 ---------------------------------------------------------------
 
-type Data = Signed 32
+systemClockPeriod = snatToInteger $ clockPeriod @System
 
-type MemorySize = 4
-type MemoryData = Data
-type Cursor = Int
-type Empty = Bool
-type Memory = Vec MemorySize MemoryData
-type MemoryState = (Memory, Cursor, Empty)
-type Push = Bool
-type Pop = Bool
-type MemoryInput = (Push, Pop, MemoryData)
-type ValidOutput = Bool
-type MemoryOutput = (Empty, ValidOutput, MemoryData)
+generateClock :: HiddenClockResetEnable dom => Integer -> Signal dom Bool
+generateClock desiredPeriodInPs = register False (mux (countSignal .<. halfToCount) (pure True) (pure False))
+  where
+    countSignal = counter toCount
+    toCount = desiredPeriodInPs `div` systemClockPeriod
+    halfToCount = pure $ toCount `div` 2
 
-defaultVal :: MemoryData
-defaultVal = 0
+    counter :: HiddenClockResetEnable dom => Integer -> Signal dom Integer
+    counter toCount = register 0 (mux (prevVal .<. pure (toCount - 1)) (prevVal + 1) 0)
+        where prevVal = counter toCount
 
-fifoFx :: MemoryState -> MemoryInput -> (MemoryState, MemoryOutput)
-fifoFx memoryState memoryInput = (nextMemoryState, output)
-    where
-        -- when we get new data the data may be available for only 1 cycle
-        -- we must immediately put the data to the queue 
-        -- so the queue must support push and pop at the same instant
-        (push, pop, memData) = memoryInput
-        (memory, cursor, isEmpty) = memoryState
-        nextMemoryState = (newMemory, newCursor, newIsEmpty)
-        output = (newIsEmpty, validOutput, outputData)
-        newMemory = case (push, pop) of
-            (True, False) -> memData +>> memory
-            (True, True) -> memData +>> memory
-            (_, _) -> memory
-        (newCursor, newIsEmpty, validOutput) = case (push, pop) of
-            (True, False) -> if cursor == length memory then (cursor, False, False) else (cursor + 1, False, False)
-            (False, True) -> if cursor == 0 || isEmpty then (cursor, True, False) else if cursor == 1 then (cursor - 1, True, True) else (cursor - 1, False, True)
-            (True, True) -> if cursor == 0 || isEmpty then (cursor + 1, False, False) else (cursor, False, True)
-            (_, _) -> (cursor, isEmpty, False)
-        outputData = case (push, pop) of
-            (False, True) -> if cursor > 0 then memory !! (cursor - 1) else head memory
-            (True, True) -> if cursor > 0 then memory !! (cursor - 1) else head memory
-            (_, _) -> memData
-
-queue :: HiddenClockResetEnable dom => Signal dom MemoryInput -> Signal dom MemoryOutput
-queue = mealy fifoFx (repeat defaultVal :: Memory, 0 :: Cursor, True :: Empty)
--- clashi Test:
--- Note: in sampleN reset is True for first 2 samples
--- assuming `type MemorySize = 4` and `type MemoryData = Data`
--- let x = fromList [(False, False, 0) :: MemoryInput, (False, False, 0), (True, False, 1), (True, False, 2), (True, False, 3), (False, True, 0), (True, False, 4), (True, False, 5), (True, False, 6), (True, False, 7), (False, True, 0), (False, True, 0), (False, True, 0), (False, True, 0), (False, True, 0)]
--- sampleN @System 15 (fifo x)
+clockDivider :: HiddenClockResetEnable dom => Integer -> Signal dom Bool
+clockDivider factor = generateClock $ systemClockPeriod * factor
 
 
 ---------------------------------------------------------------
@@ -65,19 +33,27 @@ hotPotato n = let s = register 0 (mux (s .==. pure (n-1)) 0 (s + 1)) in s
 
 ---------------------------------------------------------------
 
--- evaluator :: HiddenClockResetEnable dom => Signal dom (Int, Bool) -> Signal dom ((Int, Bool), (Int, Bool), (Int, Bool))
--- evaluator input0 = bundle (output0, output1, output2)
---     where
---         output0 = bundle (a, aktv_a)
---         output1 = bundle (b, aktv_b)
---         output2 = bundle (c, aktv_c)
---         (x, aktv_x) = unbundle input0
---         (a, aktv_a) = unbundle $ evaluateA enA (bundle (x, c))
---         (b, aktv_b) = unbundle $ evaluateB enB a
---         (c, aktv_c) = unbundle $ evaluateC enC b
+type Data = Int
+
+slowClock :: HiddenClockResetEnable dom => Signal dom Bool
+slowClock = clockDivider 4
+
+fastClock :: HiddenClockResetEnable dom => Signal dom Bool
+fastClock = clockDivider 1
+
+-- We need to sustain control signals and data from HLC until all layers are completely evaluated and outputed
+-- Hence, the LLC should run (n+1) times faster than HLC, where n = number of evaluation layers
+-- We evaluate all the layers and in the end output the values together, hence +1 in (n+1)
+-- By sustaining the HLC controls like this we also don't need any buffer or queue to interface them
+evaluator :: HiddenClockResetEnable dom => Signal dom (Data, Bool) -> Signal dom ((Data, Bool), (Data, Bool), (Data, Bool))
+evaluator input0 = outputs
+    where
+        outputs = llc fastClock controls stage
+        controls = hlc slowClock input0
+        stage = hotPotato 4
 
 
-hlc :: HiddenClockResetEnable dom => Signal dom Bool -> Signal dom (Int, Bool) -> Signal dom (Int, Bool, Bool, Bool)
+hlc :: HiddenClockResetEnable dom => Signal dom Bool -> Signal dom (Data, Bool) -> Signal dom (Data, Bool, Bool, Bool)
 hlc en input0 = register (0, False, False, False) (mux en newResult oldResult)
     where
         oldResult = hlc en input0
@@ -87,38 +63,40 @@ hlc en input0 = register (0, False, False, False) (mux en newResult oldResult)
         enB = aktv_x
         enC = aktv_x
 
--- We need to sustain control signals and data from HLC until all layers are completely evaluated and outputed
--- Hence, the LLC should run (n+1) times faster than HLC, where n = number of evaluation layers
--- We evaluate all the layers and in the end output the values together, hence +1 in (n+1)
--- By sustaining the HLC controls like this we also don't need any buffer or queue to interface them
-llc :: HiddenClockResetEnable dom => Signal dom Bool -> Signal dom (Int, Bool, Bool, Bool) -> Signal dom Int -> Signal dom ((Int, Bool), (Int, Bool), (Int, Bool))
+
+llc :: HiddenClockResetEnable dom => Signal dom Bool -> Signal dom (Data, Bool, Bool, Bool) -> Signal dom Data -> Signal dom ((Data, Bool), (Data, Bool), (Data, Bool))
 llc en controls stage = bundle (resultA, resultB, resultC)
     where 
         resultA = bundle (outA, aktvA)
         resultB = bundle (outB, aktvB)
         resultC = bundle (outC, aktvC)
-        outA = evaluateA ((stage .==. pure 1) .&&. enA) (bundle (x, outC))
+        outA = evaluateA ((stage .==. pure 1) .&&. enA) (bundle (x, outC'))
         outB = evaluateB ((stage .==. pure 2) .&&. enB) outA
-        outC = evaluateC ((stage .==. pure 3) .&&. enC) outB
+        (outC', outC) = unbundle $ evaluateC ((stage .==. pure 3) .&&. enC) outB
         (aktvA, aktvB, aktvC) = unbundle $ mux (stage .==. pure 0) (bundle (enA, enB, enC)) (bundle (pure False, pure False, pure False))
         (x, enA, enB, enC) = unbundle controls
 
 
 
-evaluateA :: HiddenClockResetEnable dom => Signal dom Bool -> Signal dom (Int, Int) -> Signal dom Int
+evaluateA :: HiddenClockResetEnable dom => Signal dom Bool -> Signal dom (Data, Data) -> Signal dom Data
 evaluateA en inpt = register 0  (mux en (x + c) oldVal)
     where 
         (x, c) = unbundle inpt 
         oldVal = evaluateA en inpt
 
-evaluateB :: HiddenClockResetEnable dom => Signal dom Bool -> Signal dom Int -> Signal dom Int
+evaluateB :: HiddenClockResetEnable dom => Signal dom Bool -> Signal dom Data -> Signal dom Data
 evaluateB en inpt = register 0 (mux en (a + 1) oldVal)
     where 
         oldVal = evaluateB en inpt
         a = inpt
 
-evaluateC :: HiddenClockResetEnable dom => Signal dom Bool -> Signal dom Int -> Signal dom Int
-evaluateC en inpt = register 0 (mux en (b + 1) oldVal)
+evaluateC :: HiddenClockResetEnable dom => Signal dom Bool -> Signal dom Data -> Signal dom (Data, Data)
+evaluateC en inpt = window2 en (b + 1) 0
     where 
-        oldVal = evaluateC en inpt
         b = inpt
+
+window2 :: HiddenClockResetEnable dom => Signal dom Bool -> Signal dom Data -> Data -> Signal dom (Data, Data)
+window2 enable x deflt = bundle (past1, cur)
+  where
+    cur = register deflt (mux enable x cur)
+    past1 = register deflt (mux enable cur past1)
